2a9b1c13547cf5503d167192214c21c078548838
Lizard Morrow <lizardmorrow@gmail.com>
2025-02-15 14:12:59 +0000

    feat: add feed
    - create component
    - add utility class for centred text
    - add primary button class

---
+++ src/app/components/add-feed/add-feed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-feed',
+  imports: [],
+  templateUrl: './add-feed.component.html',
+  styleUrl: './add-feed.component.scss',
+  standalone: true
+})
+export class AddFeedComponent {
+
+}
