294c2eeb7b9f31e0770cbde20bff776815fd2c66
Lizard Morrow <lizardmorrow@gmail.com>
2025-02-17 20:50:36 +0000

    feat: app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FeedService} from "./services/feed.service";
+import {SubscriptionUtils} from "./utils/subscription-utils/subscription-utils.component";
 
 @Component({
     selector: 'app-root',
@@ -6,4 +8,19 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss'],
     standalone: false
 })
-export class AppComponent {}
+export class AppComponent extends SubscriptionUtils implements OnInit {
+  constructor(
+    public readonly feedService: FeedService
+  ) {
+    super();
+  }
+
+  /**
+   * Fetches the content from the rss api
+   */
+  public ngOnInit() {
+    this._subs.add(
+      this.feedService.fetchContent().subscribe()
+    );
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {provideHttpClient} from "@angular/common/http";
+import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import {ContentDisplayComponent} from "./components/content-display/content-display.component";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SidebarComponent,
+    ContentDisplayComponent
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
