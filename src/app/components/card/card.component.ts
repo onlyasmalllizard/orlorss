d4855485552214f2a236a97d9531c4ddc8bf7542
Lizard Morrow <lizardmorrow@gmail.com>
2025-02-17 21:41:24 +0000

    feat: card
    - add missing icon
    - add aria hidden to image
    - make date smaller

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,17 +1,23 @@
 import {Component, Input} from '@angular/core';
 import {Article} from "../../models/article.model";
 import {DatePipe} from "@angular/common";
+import {FaIconComponent, IconDefinition} from "@fortawesome/angular-fontawesome";
+import {faArrowUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-card',
   imports: [
-    DatePipe
+    DatePipe,
+    FaIconComponent
   ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
   standalone: true
 })
 export class CardComponent {
+  /** The icon to display next to the link to the article */
+  public externalLinkIcon: IconDefinition = faArrowUpRightFromSquare;
+
   /** The article to display on the card */
   @Input() public article!: Article;
 }
